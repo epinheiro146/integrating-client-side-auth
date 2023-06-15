4abdc77b6ebc3a75af5d4cd304b78e45ea0ba6ac
epinh <epinheiro146@gmail.com>
2023-06-14 20:49:15 -0500

    adding health check endpoint

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -4,6 +4,11 @@ import chirpsRouter from "./chirps";
 
 const router = express.Router();
 
+router.use("/health", (req, res) => {
+    const timestamp = new Date().toLocaleString();
+    res.status(200).json({ message: "Nice. Server is working.", timestamp });
+});
+
 router.use("/users", usersRouter);
 router.use("/chirps", chirpsRouter);
 
